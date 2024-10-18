d058246cba45dd40e96d5b82c6c5e28a32b74e20
JanHunar <jan.hunar.jha@gmail.com>
2024-10-18 09:55:01 +0200

    ExcScreen and firebase

--- src/Components/ExcerciseItem.jsx
+++ src/Components/ExcerciseItem.jsx
@@ -1,48 +1,55 @@
-import React from 'react';
-import { StyleSheet, View, Text, TouchableOpacity, ImageBackground } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  View,
+  Text,
+  TouchableOpacity,
+  ImageBackground,
+} from "react-native";
+import { FlashList } from "@shopify/flash-list";
 
-import excData from '../../exercise_data.json';
-import { FlashList } from '@shopify/flash-list';
-
-const excImage = require('../../assets/Images/exercise1.jpg');
+import workoutData from "../../exercise_data.json";
+const exerciseImage = require("../../assets/Images/exercise1.jpg");
 
 const ExcerciseItem = () => {
-
-
-    const renderWorkoutItem = ({item}) => {
-        <TouchableOpacity>
-            <ImageBackground
-            source={excImage}
-            className="h-44 w-40 rounded-2xl overflow-hidden my-2"
-            >
-                <View>
-                    <Text>{item.category}</Text>
-                    <Text>{item.title}</Text>
-                </View>
-            </ImageBackground>
-        </TouchableOpacity>
-    }
-
+  const renderWorkoutItem = ({ item }) => (
+    <TouchableOpacity>
+      <ImageBackground
+        source={exerciseImage}
+        className="h-44 w-40 rounded-2xl overflow-hidden my-2"
+      >
+        <View className="justify-between flex-1 m-3">
+          <Text className="text-white font-medium tracking-widest">
+            {item.category}
+          </Text>
+          <Text className="text-white font-medium tracking-widest">
+            {item.title}
+          </Text>
+        </View>
+      </ImageBackground>
+    </TouchableOpacity>
+  );
 
   const renderRow = ({ item, index }) => {
     if (index % 2 === 0) {
-      const nextItem = excData[index + 1];
+      const nextItem = workoutData[index + 1];
       return (
-        <View>
-            {renderWorkoutItem({item})}{nextItem && renderWorkoutItem({item: nextItem})}
+        <View className="flex-row justify-around">
+          {renderWorkoutItem({ item })}
+          {nextItem && renderWorkoutItem({ item: nextItem })}
         </View>
-      )
+      );
     }
   };
 
   return (
-    <View>
+    <View className="min-h-screen">
       <FlashList
-        data={excData}
+        data={workoutData}
         renderItem={renderRow}
         keyExtractor={(item) => item.id}
         showsVerticalScrollIndicator={false}
-        estimatedItemSize={40}
+        estimatedItemSize={200}
       />
     </View>
   );
--- src/Screens/WorkoutScreen.jsx
+++ src/Screens/WorkoutScreen.jsx
@@ -1,21 +1,23 @@
-import React from 'react';
-import { View } from 'react-native';
-import Welcome from '../Components/Welcome';
-import { SafeAreaView } from 'react-native-safe-area-context';
-import WorkoutOTD from '../Components/WorkoutOTD';
-import Separator from '../Components/Separator';
-import Category from '../Components/Category';
-import Excercise from '../Components/Excercise';
+import React from "react";
+import { ScrollView, View } from "react-native";
+import Welcome from "../Components/Welcome";
+import { SafeAreaView } from "react-native-safe-area-context";
+import WorkoutOTD from "../Components/WorkoutOTD";
+import Separator from "../Components/Separator";
+import Category from "../Components/Category";
+import Excercise from "../Components/Excercise";
 
 const WorkoutScreen = () => {
   return (
     <SafeAreaView className="mx-[1%] ">
       <Welcome />
-      <WorkoutOTD />
-      <Separator />
-      <Category />
-      <Separator />
-      <Excercise />
+      <ScrollView>
+        <WorkoutOTD />
+        <Separator />
+        <Category />
+        <Separator />
+        <Excercise />
+      </ScrollView>
     </SafeAreaView>
   );
 };
